309697494ddf55c86581fb4e6ea117db695cfb69
Nishad Kindre <nishadkindre9553@gmail.com>
2022-04-02 21:55:36 +0530

    Update modules.py

--- ModuleInfo/modules.py
+++ ModuleInfo/modules.py
@@ -1,10 +1,12 @@
 '''
-Making module objects using the Classes from module_constructor by providing the module name as a string and respective module_data object from modules_data.
+Making module objects using the Classes from module_constructor 
+By providing the module name as a string and respective module_data object from modules_data.
 '''
 
 from module_constructor import *
-
-from modules_data import schedule_data , random_data , numpy_data , time_data , pandas_data , os_data , collections_data , datetime_data
+from modules_data import \
+schedule_data , random_data , numpy_data , time_data , \
+pandas_data , os_data , collections_data , datetime_data
 
 datetime = Module("datetime" , datetime_data)
 
