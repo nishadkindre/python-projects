7b7036e7c9bed61fcd17fdeecc1882cb28fd9e60
Nishad Kindre <nishadkindre9553@gmail.com>
2022-04-03 22:02:50 +0530

    Update modules.py

--- ModuleInfo/modules.py
+++ ModuleInfo/modules.py
@@ -6,20 +6,20 @@ By providing the module name as a string and respective module_data object from 
 from module_constructor_ import *
 import modules_data as m
 
-datetime = ModuleC(name='datetime', data=m.DATETIME)
+datetime = ModuleC(name='datetime', data=m.DATETIME_DATA)
 
-collection = ModuleC('collections', m.COLLECTIONS)
+collection = ModuleC('collections', m.COLLECTIONS_DATA)
 
-schedule = Module('schedule', m.SCHEDULE)
+schedule = Module('schedule', m.SCHEDULE_DATA)
 
-random = Module('random', m.RANDOM)
+random = Module('random', m.RANDOM_DATA)
 
-numpy = Module('numpy', m.NUMPY)
+numpy = Module('numpy', m.NUMPY_DATA)
 
-time = Module('time', m.TIME)
+time = Module('time', m.TIME_DATA)
 
-pandas = Module('pandas', m.PANDAS)
+pandas = Module('pandas', m.PANDAS_DATA)
 
-os = Module('os', m.OS)
+os = Module('os', m.OS_DATA)
 
-math = Module('math', m.MATH)
+math = Module('math', m.MATH_DATA)
