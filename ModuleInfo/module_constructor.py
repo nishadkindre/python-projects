4d9f5b879324d344eddd6b3648abbc902b21caf0
Nishad Kindre <nishadkindre9553@gmail.com>
2022-04-07 10:10:00 +0530

    Update module_constructor.py

--- ModuleInfo/module_constructor.py
+++ ModuleInfo/module_constructor.py
@@ -14,10 +14,9 @@ class ModuleC:
     With methods defined under it to return the data about the 
     module, classes under it, methods under the classes, all info, export info
     
-    Using a combine object(preferably a zip object converted to a tuple)
+    Using a combine object(preferably a zip object converted to a tuple) 
     containing all the data to be returned using the methods hereby defined.
     '''
-
     def __init__(self, name: str, data:  tuple) -> None:
         '''Necessary to define the module name and module data during object instantiation.
         
@@ -54,17 +53,14 @@ class ModuleC:
             print(X)
             class_name: str = self.data[a-1][0]
             class_info: str = self.data[a-1][1]
-
-            if a<=0:
+            if a<1:
                 print(RED)
                 print('***Please Enter Only Positive Numbers***')
             else: 
                 print('Class', class_name, '\n')
                 print('Info :\n', class_info, '\n')
-                return a
-                # refer : https://stackoverflow.com/questions/20768856/calling-a-variable-from-one-function-to-another-function-in-python 
-
-        except ValueError:
+                
+        except (ValueError):
             print(RED)
             print('***Please Enter In Number Format Only***')
 
@@ -72,25 +68,28 @@ class ModuleC:
             print(RED)
             print('***Please Enter Number within range***')
 
+        else :
+            return a
+            #  refer : https://stackoverflow.com/questions/20768856/calling-a-variable-from-one-function-to-another-function-in-python 
+
+
     def class_methods_list(self) -> int:
         '''Returns All the Methods Under Specific Class of the Module.'''
         
         # refer : https://devnote.in/how-to-call-one-method-from-another-within-the-same-class-in-python/
         self.classes_list()
-        b: int = self.class_info()
-        class_name: str = self.data[b-1][0]
-        class_methods: tuple = self.data[b-1][2]
         try:         
-            print(X)
-            print(f'Methods under Class {class_name} :')
-            for y, z in enumerate(class_methods, 1): 
-                print(y, z)
-
-        except Exception:
-            print('.')  # prints when an exception occurs in the method called
-
-        else:
-            return b
+            b: int = self.class_info()
+            class_name = self.data[b-1][0]
+            class_methods: tuple = self.data[b-1][2]
+            if b>0:
+                print(X)
+                print(f'Methods under Class {class_name} :')
+                for y, z in enumerate(class_methods, 1) : 
+                    print(y, z)
+                return b
+        except TypeError:
+            print()
 
     def class_method_info(self) -> None:
         '''Returns the information about the specific method of any class under the Module.
@@ -104,23 +103,22 @@ class ModuleC:
             number = input()
             d = int(number)
             print(X)
-            method_name: str = self.data[c-1][2][d-1]
-            method_info: str = methods_info_list[d-1]
             methods_info_list: tuple = self.data[c-1][3]
-
-            if d not in range(1, len(methods_info_list)+1):
-                print(RED)
-                print('***Please Enter A Positive Number within range***') 
-            else:
-                print(method_name, 'method :\n')
-                print(method_info) 
+            method_name: str = self.data[c-1][2][d-1]
+            method_info: str = methods_info_list[d-1] 
+            print(method_name, 'method :\n')
+            print(method_info) 
             
         except ValueError:
             print(RED)
             print('***Please Enter In Number Format Only***')
 
-        except Exception :
-            print('***Enter Correct Class Number to proceed!***')  # prints when exception/s occurs in the methods called
+        except IndexError:
+            print(RED)
+            print('***Please Enter A Positive Number within range***')
+
+        except TypeError:
+            print('***Enter Correct Class Number to proceed!***')  
 
     def all_info(self) -> None:  
         '''Returns All Classes and Methods Under the Module along with their information.'''
@@ -145,13 +143,13 @@ class ModuleC:
                 print(method_info, '\n')
             print(X)
 
-    def print_output(self, input_: str = 'self.all_info()') -> str:  # returns a str value , default parameter = 'self.all_info()'
+    def print_output(self, input_: str = 'self.all_info()') -> str:  # default parameter = 'self.all_info()'
         '''Helper Method.
 
         Return the output of an input(a function in our case) taken as an argument.
         For special purpose of printing output in a text file.
 
-        Special Method for use in export_info() method.
+        Special Method for use in the export_info() method.
         '''
         import io
         import sys
@@ -238,6 +236,7 @@ class Module(ModuleC):
     '''Constructing a 'Module' class. 
     
     With methods defined under it to return the data about the module,and the methods under it.
+    
     Using a combine object(preferably a zip object converted to a tuple) 
     containing all the data to be returned using the methods hereby defined.
     
@@ -258,13 +257,12 @@ class Module(ModuleC):
         Returns the information about the specific Module method.
         Using the input number given by the user as an argument while calling the function.
 
-        Special Method for use in method_info() & all_info() method.
+        Special Method made for use in the method_info() & all_info() method.
         '''
         x: str = '\n' + '-'*50
         self.number = number
         method_name: str = self.data[self.number-1][0] + '()'
         method_info: str = self.data[self.number-1][1]
-
         print(method_name, 'Method :')
         print(method_info)
         print(x)
@@ -276,8 +274,12 @@ class Module(ModuleC):
         '''
         try:        
             number = int(input('Enter Method number : '))
-            print(X)
-            self.method_info_(number)
+            if number<1:
+                print(RED)
+                print('***Please Enter Only Positive Numbers***')
+            else:
+                print(X)
+                self.method_info_(number)
 
         except ValueError:
             print(RED)
@@ -289,13 +291,12 @@ class Module(ModuleC):
 
     def all_info(self) -> None:
         '''Returns All Methods Under the Module along with their information.'''
-        all_methods_info = tuple(map(lambda a: self.method_info_(a), range(1, len(self.data)+1)))
-        
         print(X)
         self.module_info()
         print(X)
         self.methods_list()
         print(X)
+        all_methods_info = tuple(map(lambda a: self.method_info_(a), range(1, len(self.data)+1)))  # if assigned at top executes below statement first.
         print(all_methods_info[:-len(self.data)])
 
     def run(self) -> None:
@@ -308,7 +309,7 @@ class Module(ModuleC):
                   \n 4 : All Info                         \
                   \n 5 : Export All Info To a Text File   \
                   \n 6 : Exit \n')
-
+            
             input_ = input('Enter Number : ')
             print(X)
 
